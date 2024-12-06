3afab80a58f86cc8c3dfe864210c8149bf36268d
Eric Wyles <23637493+ericwyles@users.noreply.github.com>
2024-12-06 05:31:50 -0600

    renaming

--- day04/ex2/main.go
+++ day04/ex2/main.go
@@ -26,7 +26,6 @@ func main() {
 	for y := 1; y < len(grid)-1; y++ {
 		for x := 1; x < len(grid[y])-1; x++ {
 			if grid[y][x] == 'A' { // keying off the middle of the pattern and will look at neighbors from here
-				// Check for matching patterns
 				if isMas(grid[y+1][x-1], grid[y][x], grid[y-1][x+1]) &&
 					isMas(grid[y-1][x-1], grid[y][x], grid[y+1][x+1]) {
 					totalFound++
